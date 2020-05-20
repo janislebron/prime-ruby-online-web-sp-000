0bef3337255ee9ca206fd57d34547d01a464436f
Janis Lebron <janislebron@gmail.com>
2020-05-20 02:20:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,12 @@
-def prime (integer)
-  if 6n+1 or 6n-1
-    true
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_to_test| #none of the numbers should return true for none? to return true. If none of the numbers
+                                # return true, then they are not divisible by the number and the number is prime
+      number % num_to_test == 0
+    end
   else
     false
+  end
 end
